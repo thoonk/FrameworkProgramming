4573774d3c7bf976b2ae665b26efc1f237bd5e05
dkrmrm878 <dkrmrm878@gmail.com>
2020-05-28 15:10:54 +0900

    Fix TransactionSampleMain.java

--- kth-transaction-mybatis/src/main/java/org/kpu/di/main/TransactionSampleMain.java
+++ kth-transaction-mybatis/src/main/java/org/kpu/di/main/TransactionSampleMain.java
@@ -13,22 +13,24 @@ public class TransactionSampleMain {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요 TRANSACTION-MYBATIS");
 	
-		ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-//		ctx = new ClassPathXmlApplicationContext("classpath:applicationContext_annotation.xml");
+//		ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		ctx = new ClassPathXmlApplicationContext("classpath:applicationContext_annotation.xml");
 		
 		MemberService memberService = ctx.getBean(MemberService.class);  // by Class name
 		
 		String strID1 = "hansol";
 		StudentVO vo1 = new StudentVO(); 
 		vo1.setId(strID1); vo1.setPasswd(strID1);  vo1.setSnum(strID1);
-		vo1.setUsername("가나다라마바사자차카타파하가나다라마바사자차카타파하");
+		vo1.setUsername("가나다라마바사아자차카타파ㅏㅎ하하하하하하");
 		
 		String strID2 = "dongjin";
 		StudentVO vo2 = new StudentVO();
-		vo2.setId(strID2); vo2.setPasswd(strID2);  vo2.setSnum(strID2);
+		vo2.setId(strID2); vo2.setPasswd("3334");  vo2.setSnum(strID2);
 		vo2.setUsername(strID2);
 		
 		try {
+			//memberService.addMember(vo1);
+			//memberService.addMember(vo2);
 			memberService.updateMemberList(vo1, vo2);
 			System.out.println("TRANSACTION 처리 완료");
 		} catch(Exception e) {
